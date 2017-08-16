21f700ef5d2288b34dee6c9916259e85d9c3b599
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-16 17:30:48 -0600

    adds food object test

---
+++ test/food_test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert
+const Food = require("../lib/javascripts/food.js")
+
+describe('test food object', function() {
+    it('should hold name and calories', function() {
+        const food = new Food('Banana', 150)
+        assert.equal(food.name, 'Banana')
+        assert.equal(food.calories, 150)
+    })
+})
