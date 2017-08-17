134c3421d37c447c1a26b023ffb2e8e6a0c1a1fd
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-17 15:10:52 -0600

    foods display separated by meals on index

--- test/diary_test.js
+++ test/diary_test.js
@@ -1,27 +1,27 @@
-var assert = require('chai').assert;
-var webdriver = require('selenium-webdriver');
-var until = webdriver.until;
-var test = require('selenium-webdriver/testing');
-var frontEndLocation = "http://localhost:8080"
+// var assert = require('chai').assert;
+// var webdriver = require('selenium-webdriver');
+// var until = webdriver.until;
+// var test = require('selenium-webdriver/testing');
+// var frontEndLocation = "http://localhost:8080"
 
-const Diary = require("../lib/diary.js")
+// const Diary = require("../lib/diary.js")
 
 
-test.describe('testing my simple blog', function() {
-    var driver;
-    this.timeout(10000);
+// test.describe('testing my simple blog', function() {
+//     var driver;
+//     this.timeout(10000);
 
-    test.beforeEach(function() {
-        driver = new webdriver.Builder()
-            .forBrowser('chrome')
-            .build();
-    });
+//     test.beforeEach(function() {
+//         driver = new webdriver.Builder()
+//             .forBrowser('chrome')
+//             .build();
+//     });
 
-    test.afterEach(function() {
-        driver.quit();
-    });
+//     test.afterEach(function() {
+//         driver.quit();
+//     });
 
-    test.it("", function() {
+//     test.it("", function() {
 
-    })
-})
+//     })
+// })
