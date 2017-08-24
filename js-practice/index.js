d79e38982df11970ca9f96793c55a86444e99939
Padraic O'Neill <pconeill@gmail.com [core]>
2017-08-24 14:08:55 -0600

    adds opacity effect

--- js-practice/index.js
+++ js-practice/index.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
     var $mealItems = $('.meal-item')
+
     $('.meal-title').on('click', function() {
         var count = 0
         var numItems = $mealItems.length
         var currentRot = 0
         var rotation = 360 / numItems
-        $mealItems.each(function(index, element) {
-            $(element).show(function() {
-                $(element).css(`transform`, `rotate(${currentRot}deg) translate(8em) rotate(-${currentRot}deg)`);
+
+        $.each($mealItems, function() {
+            $(this).show(function() {
+                $(this).css(`transform`, `rotate(${currentRot}deg) translate(8em) rotate(-${currentRot}deg)`).css('animation', 'fadeIn 2s forwards');
                 currentRot = currentRot + rotation
             });
         });
+
     });
 
     let rotationState = 'paused'
